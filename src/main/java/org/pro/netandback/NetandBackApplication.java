ab246687770e19f8a0c62bbf3e636347e0e0570d
박지훈 <jihun6548@naver.com>
2025-06-02 10:33:35 +0900

    option: Gradle의존성 설정

---
+++ src/main/java/org/pro/netandback/NetandBackApplication.java
@@ -0,0 +1,13 @@
+package org.pro.netandback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetandBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetandBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pro/netandback/NetandBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pro.netandback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetandBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
