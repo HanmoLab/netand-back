cc59069a8edc3256bf3ab44a47f253c16cc75ecc
박지훈 <jihun6548@naver.com>
2025-06-02 14:01:03 +0900

    feat: Web,Swagger,QueryDsl Config추가

---
+++ src/main/java/org/pro/netandback/core/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package org.pro.netandback.core.config;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+public class SwaggerConfig {
+	private SecurityScheme createBearerAuthScheme() {
+		return new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.bearerFormat("JWT")
+			.scheme("bearer");
+	}
+
+	private OpenApiCustomizer createOpenApiCustomizer(String title, String version) {
+		return openApi -> {
+			openApi.info(new Info().title(title).version(version));
+			openApi.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+			openApi.schemaRequirement("bearerAuth", createBearerAuthScheme());
+		};
+	}
+
+	@Bean
+	public GroupedOpenApi allApi() {
+		return GroupedOpenApi.builder()
+			.group("all")
+			.pathsToMatch("/**")
+			.displayName("All API")
+			.addOpenApiCustomizer(createOpenApiCustomizer("모든 API", "v0.4"))
+			.build();
+	}
+}
---
+++ src/main/java/org/pro/netandback/core/config/WebConfig.java
@@ -0,0 +1,18 @@
+package org.pro.netandback.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	//아직 배포 전이라 열어둡니다.
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("*")
+			.allowedHeaders("*");
+	}
+}
