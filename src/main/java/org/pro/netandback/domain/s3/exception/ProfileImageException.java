200743dd5df97391b0169b88fbc7c997a726d731
박지훈 <jihun6548@naver.com>
2025-06-12 10:45:57 +0900

    feat: 프로필 이미지 예외 처리를 위한 ErrorCode 및 ProfileImageException 추가

--- src/main/java/org/pro/netandback/domain/s3/exception/ProfileImageException.java
+++ src/main/java/org/pro/netandback/domain/s3/exception/ProfileImageException.java
@@ -1,4 +1,10 @@
 package org.pro.netandback.domain.s3.exception;
 
-public class ProfileImageException {
+import org.pro.netandback.core.error.ErrorCode;
+import org.pro.netandback.core.error.exception.BusinessException;
+
+public class ProfileImageException extends BusinessException {
+	public ProfileImageException(ErrorCode errorCode) {
+		super(errorCode);
+	}
 }
