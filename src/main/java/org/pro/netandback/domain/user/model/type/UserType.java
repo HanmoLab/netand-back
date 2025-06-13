d5902e3121237c2667bdc60b88d6f3e090f132d4
dahye <dahye9011@naver.com>
2025-06-13 16:43:53 +0900

    feat: UserType 세분화

--- src/main/java/org/pro/netandback/domain/user/model/type/UserType.java
+++ src/main/java/org/pro/netandback/domain/user/model/type/UserType.java
@@ -1,7 +1,8 @@
 package org.pro.netandback.domain.user.model.type;
 
 public enum UserType {
-	INTERNAL,   // 내부 사용자 (넷앤드 내 엔지니어 등)
-	PARTNER,    // 파트너사 엔지니어 등
-	ADMIN       // 시스템 관리자
+	NETAND_REPORTER,	// 넷앤드 소속 이슈 발의 담당자
+	NETAND_ENGINEER,	// 넷앤드 엔지니어 (실제 개발/조치 담당)
+	PARTNER_ENGINEER,	// 파트너사 엔지니어 (실제 개발/조치 담당)
+	ADMIN       		// 시스템 관리자
 }
