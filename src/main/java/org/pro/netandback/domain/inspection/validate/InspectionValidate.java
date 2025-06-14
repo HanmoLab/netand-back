1e6a0f37507fb36d32c5c2fc6d7c2c388229788a
박지훈 <jihun6548@naver.com>
2025-06-14 14:42:40 +0900

    feat: 조회·예외처리 로직 분리

--- src/main/java/org/pro/netandback/domain/inspection/validate/InspectionValidate.java
+++ src/main/java/org/pro/netandback/domain/inspection/validate/InspectionValidate.java
@@ -1,4 +1,21 @@
 package org.pro.netandback.domain.inspection.validate;
 
+import org.pro.netandback.core.error.ErrorCode;
+import org.pro.netandback.domain.inspection.exception.InspectionNotFoundException;
+import org.pro.netandback.domain.inspection.model.entity.Inspection;
+import org.pro.netandback.domain.inspection.repository.InspectionRepository;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class InspectionValidate {
+
+	private final InspectionRepository inspectionRepository;
+
+	public Inspection validateInspection(Long inspectionId) {
+		return inspectionRepository.findById(inspectionId)
+			.orElseThrow(() -> new InspectionNotFoundException(ErrorCode.INSPECTION_NOT_FOUND));
+	}
 }
