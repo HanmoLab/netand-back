6b6ace0b04676ce4c9d345b780a4bafa4a55052b
박지훈 <jihun6548@naver.com>
2025-07-02 00:59:25 +0900

    hotfix(repository): 잘못된 메서드명으로 인한 빈 생성 실패 수정

--- src/main/java/org/pro/netandback/domain/issue/repository/IssueRepository.java
+++ src/main/java/org/pro/netandback/domain/issue/repository/IssueRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+
+    @Query("SELECT i FROM Issue i")
     @EntityGraph(attributePaths = {"company", "product", "assignee"})
     Page<Issue> findAllWithCompanyAndProduct(Pageable pageable);
 }
