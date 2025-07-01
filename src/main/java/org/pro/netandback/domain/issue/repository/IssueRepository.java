b0e59d035eb618b549897975c0a9f6ece0c16e7c
dahye <dahye9011@naver.com>
2025-07-01 10:00:03 +0900

    fix: 연관 엔티티 회사/제품 정보 누락 오류 수정

--- src/main/java/org/pro/netandback/domain/issue/repository/IssueRepository.java
+++ src/main/java/org/pro/netandback/domain/issue/repository/IssueRepository.java
@@ -2,10 +2,14 @@ package org.pro.netandback.domain.issue.repository;
 
 
 import org.pro.netandback.domain.issue.model.entity.Issue;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, Long> {
-
+    @EntityGraph(attributePaths = {"company", "product", "assignee"})
+    Page<Issue> findAllWithCompanyAndProduct(Pageable pageable);
 }
